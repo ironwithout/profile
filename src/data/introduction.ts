1e914d6848952c756737794695164538fed882a0
ironwithout <lupus.vertex@gmail.com>
2026-01-08 17:51:46 +0100

    style: improve design

--- src/data/introduction.ts
+++ src/data/introduction.ts
@@ -3,5 +3,7 @@
  */
 export const introduction = {
   name: "Miguel Sánchez Deleyto",
-  description: "I’m a Senior DevOps Engineer with a background in both platform engineering and application development. I design and operate cloud-native infrastructure and CI/CD systems with a strong focus on automation, reliability, and cost efficiency. Over the years, I’ve helped teams ship faster and more predictably by turning complex delivery pipelines into scalable, data-driven platforms.",
-};
+  description: "I’m a Senior DevOps Engineer with a background in both platform engineering and application development. I design and operate cloud-native infrastructure and CI/CD systems with a strong focus on automation, reliability, and cost efficiency. Over the years, I’ve helped teams ship faster and more predictably by turning complex delivery pipelines into scalable, data-driven platforms.",  social: {
+    github: "https://github.com/ironwithout/",
+    linkedin: "https://www.linkedin.com/in/miguel-sanchez-devops/",
+  }};
--- src/data/skills.ts
+++ src/data/skills.ts
@@ -3,5 +3,7 @@
  */
 export const skills = {
   Languages: ["Java/Kotlin", "Ruby", "Python", "Shell Scripting"],
-  "DevOps & Cloud": ["AWS", "Docker", "Kubernetes", "Helm", "Jenkins", "GH Actions", "ArgoCD"],
+  Databases: ["MysQL", "InfluxDB"],
+  "DevOps & Cloud": ["AWS", "Docker", "Kubernetes", "Helm", "Jenkins", "GH Actions", "ArgoCD", "Terraform", "Ansible"],
+  Tools: ["Git", "Copilot", "VSCode", "IntelliJ IDEA"],
 };
